1ac2984770bebae423c861eb6c58d7a23c8261ae
Yehor Bukhanets <Yehor.Bukhanets@infopulse.com>
2022-11-29 19:16:27 +0200

    Lab 3 implemented

---
+++ app/src/main/java/com/example/lines/model/Color.java
@@ -0,0 +1,14 @@
+package com.example.lines.model;
+
+public enum Color {
+    RED("red"),
+    ORANGE("orange"),
+    YELLOW("yellow"),
+    GREEN("green"),
+    BLUE("blue"),
+    INDIGO("indigo"),
+    VIOLET("violet");
+
+    Color(String name) {
+    }
+}
